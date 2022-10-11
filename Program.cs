cea2a6cedb69db25c83aef9e392c7def6b993ff8
Natali <n7483205@ya.ru>
2022-10-11 19:54:08 +0700

    distance btw 2 points

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿void GetDistanceBetweenTwoPoints(int firstX, int secondX, int firstY, int secondY, int firstZ, int secondZ)
+{
+    double result = 0;
+    double coordinateX = Math.Pow(secondX - firstX, 2);
+    double coordinateY = Math.Pow(secondY - firstY, 2);
+    double coordinateZ = Math.Pow(secondZ - firstZ, 2);
+
+    result = Math.Sqrt(coordinateX + coordinateY + coordinateZ);
+
+    Console.WriteLine($"Расстояние между координатами = {result}");
+}
+
+GetDistanceBetweenTwoPoints(7, 1, -5, -1, 0, 9);
